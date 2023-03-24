834d0022774cc3da9f32df0c00e1f02fb1e551b8
smart_nishida <nishida@smarttech.co.jp>
2023-03-24 09:43:34 +0900

    provider_page.dartのファイル移動

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'provider/provider_page.dart';
+import 'provider/provider_page/provider_page.dart';
 import 'provider/state_notifier_provider_page/state_notifier_provider_page.dart';
 
 void main() {
